d941f6df9537d24647cfe9eb5bc4a4bb7b4793cf
HP <sewwandigunarathna77@gmail.com>
2024-05-15 11:32:34 -0700

    Finish Project

---
+++ app/src/main/java/com/example/attendence_application/StudentItem.java
@@ -0,0 +1,46 @@
+package com.example.attendence_application;
+
+public class StudentItem {
+    private long sid;
+    private String roll;
+    private String name;
+    private String status;
+
+    public StudentItem(long sid, String roll, String name){
+        this.sid = sid;
+        this.roll = roll;
+        this.name = name;
+        status = "";
+    }
+    public String getRoll() {
+        return roll;
+    }
+
+    public void setRoll(String roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public long getSid() {
+        return sid;
+    }
+
+    public void setSid(long sid) {
+        this.sid = sid;
+    }
+}
